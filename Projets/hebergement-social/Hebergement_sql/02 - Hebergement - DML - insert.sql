c50785629de4d1c06193a392a5ec694bbe8fd77b
CRM202A <mdevoldere@users.noreply.github.com>
2023-02-14 12:33:08 +0100

    debut requetes

--- Projets/hebergement-social/Hebergement_sql/02 - Hebergement - DML - insert.sql
+++ Projets/hebergement-social/Hebergement_sql/02 - Hebergement - DML - insert.sql
@@ -24,20 +24,20 @@ VALUES
 ('Neymar', 'Jean', '1977-12-06', 7),
 ('Noah', 'Yannick', '1965-01-02', 6),
 ('Faronne', 'Sarah', '1985-07-04', 6),
-('Holems', 'Cathy', '1990-07-21', 3),
-('Mannheim', 'Luc', '1982-07-14', 3),
-('Kipré', 'Abdou', '1982-03-16', 3),
-('Martin', 'Valérie', '1984-11-02', 4),
+('Holems', 'Cathy', '1990-07-21', 3), -- 6
+('Mannheim', 'Luc', '1982-07-14', 3), -- 7
+('Kipr?', 'Abdou', '1982-03-16', 3), -- 8
+('Martin', 'Val?rie', '1984-11-02', 4),
 ('Deloin', 'Alain', '1981-11-22', 4),
 ('Bahl', 'Tarik', '1990-04-02', 4),
 ('Goldman', 'Estelle', '1992-01-23', 4),
-('Durand', 'Gabriel', '1990-10-17', 5); -- résident
+('Durand', 'Gabriel', '1990-10-17', 5); -- r?sident
 
 -- V2
 INSERT INTO Personnes
 (personne_nom, personne_prenom, personne_ddn, role_id) 
 VALUES 
-('Nixon', 'Tiger', '2005-12-08', 5),
+('Nixon', 'Tiger', '2005-12-08', 5), -- 14
 ('Garett', 'Winters', '2004-05-19', 5),
 ('Ashton', 'Cox', '2003-05-14', 5),
 ('Cedric', 'Kelly', '2002-02-22', 5),
@@ -47,7 +47,23 @@ VALUES
 ('Rhona', 'Davidson', '2006-10-30', 5),
 ('Frost', 'Sonya', '2006-02-28', 5),
 ('Gaines', 'Jena', '2000-06-05', 5),
-('Flynn', 'Quinn', '2002-06-12', 5);
+('Flynn', 'Quinn', '2002-06-12', 5); --24
+
+INSERT INTO Residents 
+(personne_id, medecin_id, medecin_consultation, resident_date_arrivee, resident_num_chambre, resident_date_visite_med) 
+VALUES 
+(14, NULL, 6, GETDATE(), 101, GETDATE()),
+(15, 7	 , 6, GETDATE(), 102, GETDATE()),
+(16, NULL, 6, GETDATE(), 103, GETDATE()),
+(17, 7	 , 6, GETDATE(), 104, GETDATE()),
+(18, NULL, 6, GETDATE(), 105, GETDATE()),
+(19, NULL, 6, GETDATE(), 201, GETDATE()),
+(20, NULL, 6, GETDATE(), 202, GETDATE()),
+(21, NULL, 6, GETDATE(), 203, GETDATE()),
+(22, NULL, 6, GETDATE(), 205, GETDATE()),
+(23, NULL, 6, GETDATE(), 206, GETDATE()),
+(24,	8, 6, GETDATE(), 207, GETDATE());
+
 
 INSERT INTO Etats 
 (etat_intitule) 
---
+++ Projets/hebergement-social/Hebergement_sql/03 - Hebergement - DQL - V1.sql
@@ -0,0 +1,97 @@
+-- 1 Sélectionner tous les éducateurs.
+
+SELECT personne_id, personne_nom, personne_prenom, role_id FROM Personnes WHERE role_id = 4;
+
+
+
+-- 2 Sélectionner tous les employés autre que le directeur.
+
+-- SELECT personne_id, personne_nom, personne_prenom, role_id FROM Personnes WHERE role_id NOT BETWEEN 1 AND 5;
+
+SELECT personne_id, personne_nom, personne_prenom, role_id FROM Personnes WHERE role_id IN (6, 7, 8);
+
+SELECT personne_id, personne_nom, personne_prenom, role_id FROM Personnes WHERE role_id = 6 OR role_id = 7 OR role_id = 8;
+
+
+
+-- 3 Sélectionner toutes les personnes triées par rôle puis par nom.
+
+SELECT personne_id, personne_nom, personne_prenom, role_id FROM Personnes 
+ORDER BY role_id ASC, personne_nom ASC;
+
+
+
+-- 4 Sélectionner tous les rôles avec le nombre de personnes associées à chaque rôle.
+
+-- SELECT COUNT(*) as nombre_personnes FROM personnes WHERE role_id = 5;
+
+SELECT 
+role_id, 
+role_nom,
+(SELECT COUNT(*) FROM personnes WHERE Personnes.role_id = Roles.role_id) as nombre_personnes
+FROM Roles;
+
+SELECT 
+COUNT(Personnes.role_id) as nombre_personnes,
+Roles.role_id,
+role_nom 
+FROM Roles 
+JOIN Personnes ON Personnes.role_id = Roles.role_id 
+GROUP BY Roles.role_id, role_nom
+ORDER BY nombre_personnes DESC;
+
+
+-- SELECTIONNER les résidents (informations complètes)
+
+SELECT 
+resident_id, 
+resident_date_arrivee, 
+resident_date_depart, 
+resident_num_chambre, 
+resident_besoins_medicaux, 
+medecin_id, 
+medecin_consultation, 
+Residents.personne_id,
+personne_nom, 
+personne_prenom
+FROM Residents, Personnes
+WHERE Personnes.personne_id = Residents.personne_id;
+
+-- idem avec jointures
+SELECT 
+resident_id, 
+resident_date_arrivee, 
+resident_date_depart, 
+resident_num_chambre, 
+resident_besoins_medicaux, 
+medecin_id, 
+medecin_consultation, 
+Residents.personne_id,
+personne_nom, 
+personne_prenom
+FROM Residents RIGHT JOIN Personnes ON Personnes.personne_id = Residents.personne_id;
+
+SELECT 
+resident_id, 
+resident_date_arrivee, 
+resident_date_depart, 
+resident_num_chambre, 
+resident_besoins_medicaux, 
+medecin_id, 
+medecin_consultation, 
+Residents.personne_id,
+personne_nom, 
+personne_prenom 
+FROM Personnes JOIN Residents ON Personnes.personne_id = Residents.personne_id;
+
+
+
+
+
+-- 5 Sélectionner tous les résidents actifs du plus jeune au plus âgé.
+
+
+-- 6 Sélectionner tous les résidents suivis par un médecin avec nom et prénom du médecin attitré.
+
+
+-- 7 Sélectionner tous les médecins avec le nombre de résidents qu’il suivent.
